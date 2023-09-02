8af091b28cc949447a5806b5a475c5763443f29d
Erik <100293508+rikjnthn@users.noreply.github.com>
2023-09-02 12:40:46 +0700

    feat(users): hash the user password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -10,11 +11,22 @@ import { UserType } from './interface/user.interface';
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
+  private async hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    const generatedSalt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, generatedSalt);
+
+    return hashedPassword;
+  }
   async create(createUserDto: CreateUserDto): Promise<UserType> {
+    const hashedPassword = await this.hashPassword(createUserDto.password);
     const [createdUser] = await this.prisma.$transaction(
       [
         this.prisma.users.create({
-          data: createUserDto,
+          data: {
+            ...createUserDto,
+            password: hashedPassword,
+          },
         }),
       ],
       { isolationLevel: Prisma.TransactionIsolationLevel.Serializable },
@@ -34,13 +46,22 @@ export class UsersService {
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<UserType> {
+    let hashedPassword: string | undefined;
+
+    if (updateUserDto.password) {
+      hashedPassword = await this.hashPassword(updateUserDto.password);
+    }
+
     const [updatedUser] = await this.prisma.$transaction(
       [
         this.prisma.users.update({
           where: {
             id,
           },
-          data: updateUserDto,
+          data: {
+            ...updateUserDto,
+            password: hashedPassword,
+          },
         }),
       ],
       { isolationLevel: Prisma.TransactionIsolationLevel.Serializable },
