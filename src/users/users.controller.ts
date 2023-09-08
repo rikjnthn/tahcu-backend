212936e409651ee8f9df986622105ea678566f3a
Erik <100293508+rikjnthn@users.noreply.github.com>
2023-09-08 11:09:12 +0700

    feat(user controller): add guard to all route except sign-up

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,12 +7,14 @@ import {
   Param,
   Delete,
   UseFilters,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaKnownRequestErrorFilter } from 'src/common/filter/prisma-known-request-error.filter';
 import { UserType } from './interface/user.interface';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('users')
 @UseFilters(PrismaKnownRequestErrorFilter)
@@ -25,11 +27,13 @@ export class UsersController {
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
   async find(@Param('id') id: string): Promise<UserType[]> {
     return await this.usersService.find(id);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   async update(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
@@ -38,6 +42,7 @@ export class UsersController {
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   async remove(@Param('id') id: string): Promise<void> {
     await this.usersService.remove(id);
   }
