475706c88f4bad674f69fdd27f30cfdb3e61afd8
Erik <100293508+rikjnthn@users.noreply.github.com>
2023-09-16 09:46:45 +0700

    refactor(auth guard): add handle websocket guard and seperate it with http

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -6,28 +6,52 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { Socket } from 'socket.io';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private jwtService: JwtService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    return context.getType() === 'http'
+      ? this.handleHttp(context)
+      : this.handleWs(context);
+  }
+
+  async handleHttp(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest<Request>();
 
     const token = request.cookies?.tahcu_auth?.access_token;
 
-    if (!token) throw new UnauthorizedException();
+    const payload = await this.verifyJwt(token);
 
+    request.user = payload;
+
+    return true;
+  }
+
+  async handleWs(context: ExecutionContext) {
+    const client = context.switchToWs().getClient<Socket>();
+
+    const token = client.handshake['cookies'].access_token;
+
+    const payload = await this.verifyJwt(token);
+
+    client.handshake.headers.user = payload;
+
+    return true;
+  }
+
+  async verifyJwt(token: string) {
+    if (!token) throw new UnauthorizedException();
     try {
       const payload = await this.jwtService.verifyAsync(token, {
         secret: process.env.JWT_SECRET,
       });
 
-      request.user = payload;
+      return payload;
     } catch {
       throw new UnauthorizedException();
     }
-
-    return true;
   }
 }
