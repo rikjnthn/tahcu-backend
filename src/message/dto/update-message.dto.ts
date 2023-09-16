2c8157095f58684096721591743ce812f1f8a773
Erik <100293508+rikjnthn@users.noreply.github.com>
2023-09-16 15:20:35 +0700

    refactor(update message dto): remove import method from class validator

--- src/message/dto/update-message.dto.ts
+++ src/message/dto/update-message.dto.ts
@@ -1,5 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMessageDto } from './create-message.dto';
-import { IsNotEmpty, IsString } from 'class-validator';
 
 export class UpdateMessageDto extends PartialType(CreateMessageDto) {}
