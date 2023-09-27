2583da06ccc435c74036d39d0a9899791ec2aade
Erik <100293508+rikjnthn@users.noreply.github.com>
2023-09-27 11:54:13 +0700

    test(users controller): add module, provider, mock value to be tested

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { CreateUserDto } from './dto/create-user.dto';
 
-describe('UsersController', () => {
-  let controller: UsersController;
+describe('UsersController Unit Test', () => {
+  let usersController: UsersController;
 
+  const userServiceMock = {
+    create: jest.fn(),
+    find: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  const createUserDtoStub: CreateUserDto = {
+    user_id: 'tes123',
+    email: 'tes@gmail.com',
+    is_active: true,
+    password: 'tes123',
+    username: 'test',
+  };
+
+  const createdUserMock = {
+    id: '1',
+    user_id: 'tes123',
+    email: 'tes@gmail.com',
+    is_active: true,
+    password: 'tes123',
+    username: 'test',
+    created_at: new Date(),
+    updated_at: new Date(),
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule],
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: userServiceMock,
+        },
+      ],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
   });
 });
